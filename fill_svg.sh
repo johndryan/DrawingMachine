304705d2ae1213867f331a06d45c03a218a840e1
John Ryan <johndryan@gmail.com>
2013-03-09 12:58:48 -0800

    Renamed / deleted unused components

--- fill_svg.sh
+++
@@ -1,4 +0,0 @@
-
-export CP=lib/RXTXcomm.jar:lib/core.jar:lib/serial.jar:bin:lib/geomerative.jar
-
-java -d32 -Djava.library.path=lib -cp $CP com.tinkerlog.kritzler.FillSvg $1
--- plotter.sh
+++
@@ -1,4 +0,0 @@
-
-export CP=lib/RXTXcomm.jar:lib/core.jar:lib/serial.jar:bin:lib/geomerative.jar:lib/controlP5.jar
-
-java -d32 -Djava.library.path=lib -cp $CP com.tinkerlog.kritzler.Plotter
